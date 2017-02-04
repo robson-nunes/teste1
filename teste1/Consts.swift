0c9052768d4645087fb8417644371ae8c2137587
Romilson Nunes <souzainf3@yahoo.com.br>
2017-02-04 17:27:29 -0200

    Cria Conts

---
+++ teste1/Consts.swift
@@ -0,0 +1,13 @@
+//
+//  Consts.swift
+//  teste1
+//
+//  Created by Romilson Nunes on 04/02/17.
+//  Copyright © 2017 ROBSON NUNES DE SOUZA. All rights reserved.
+//
+
+import UIKit
+
+struct Consts {
+    static let tintColor = UIColor.black
+}
